9d6b9ccdf4e98c4efddece3119d3d66590fc87b1
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-03-26 19:56:15 +0530

    Add files via upload

---
+++ Constant/1 Constant Variable of Program.cpp
@@ -0,0 +1,29 @@
+using namespace std;
+
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+
+class Element
+{
+    public:
+
+        const int j;
+
+        void Ele(int);
+};
+
+void Element::Ele(int x)
+{
+    x = 5;
+
+}
+
+
+int main()
+{
+    x++;
+
+    getch();
+    return 0;
+}
